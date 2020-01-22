d28604bae10bc9c75c2e04620603a6fdba5e0f58
Jack Griffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2020-01-22 14:52:14 +0000

    S1C7 - Line Renderer for Pointing & Handedness
    Handedness consideration added. This will, in the future, allow the player to choose which hand they use most and the game will handle accordingly. A line renderer has also been added for the point and click functionality.

--- High Ground VR/Assets/Scripts/InputManager.cs
+++ High Ground VR/Assets/Scripts/InputManager.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     private static InputManager s_instance;
-
-    [SerializeField] private GameObject m_vrRig, m_camera, m_leftController, m_rightController;
+    private enum HandTypes{left,right};
+    [SerializeField] private HandTypes m_handedness;
+    [SerializeField, Space(20)] private GameObject m_vrRig;
+    [SerializeField]private GameObject m_camera, m_leftController, m_rightController,m_gameEnvironment;
 
     [Header("Player Scaling")]
     [SerializeField,Space(20)] private Vector3 m_smallestScale;
     [SerializeField] private Vector3 m_largestScale;
     [SerializeField] private float m_scalingSpeed;
+    private float m_maxWorldHeight; //This depends on the player's height.
+
+
+    [Header("Point & Click")]
+    [SerializeField, Space(20)] private Material m_pointerMaterial;
 
     private bool m_leftGripped, m_rightGripped, m_leftTrigger,m_rightTrigger;
     private Vector3 m_prevControllerMidpoint, m_controllerMidpoint;
     private float m_prevControllerDistance, m_controllerDistance;
+
+    private GameObject m_mainController, m_offHandController;
+    private LineRenderer m_mainPointer;
 
     #region Accessors
     public static InputManager Instance { get => s_instance; set => s_instance = value; }
@@ -23,12 +33,35 @@ public class InputManager : MonoBehaviour
     public bool RightGripped { get => m_rightGripped; set => m_rightGripped = value; }
     public bool LeftTrigger { get => m_leftTrigger; set => m_leftTrigger = value; }
     public bool RightTrigger { get => m_rightTrigger; set => m_rightTrigger = value; }
+    public GameObject OffHandController { get => m_offHandController; set => m_offHandController = value; }
+    public GameObject MainController { get => m_mainController; set => m_mainController = value; }
     #endregion
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //Handles whether the player is left or right handed.
+        if (m_handedness == HandTypes.left)
+        {
+            m_mainController = m_leftController;
+            m_offHandController = m_rightController;
+        }
+        if (m_handedness == HandTypes.right)
+        {
+            m_mainController = m_rightController;
+            m_offHandController = m_leftController;
+        }
+
+        //Adding a line renderer for the point & click functionality
+        m_mainPointer = m_mainController.AddComponent<LineRenderer>();
+        m_mainPointer.enabled = true;
+        m_mainPointer.SetPosition(0, m_mainController.transform.position);
+        m_mainPointer.SetPosition(1, m_mainController.transform.forward * 100);
+        m_mainPointer.startWidth = 0.05f;
+        m_mainPointer.endWidth = 0.00f;
+        m_mainPointer.material = m_pointerMaterial;
+
+        m_maxWorldHeight = m_camera.transform.position.y * 0.7f;
+        m_gameEnvironment.transform.position = new Vector3(m_gameEnvironment.transform.position.x, m_maxWorldHeight, m_gameEnvironment.transform.position.z);
     }
 
     void Awake()
@@ -45,11 +78,9 @@ public class InputManager : MonoBehaviour
         }
     }
 
-
-
-    // Update is called once per frame
     void Update()
     {
+        #region Player Movement Handling
         //Draw a line between the two controllers.
         Debug.DrawLine(m_leftController.transform.position, m_rightController.transform.position,Color.red);
 
@@ -71,9 +102,9 @@ public class InputManager : MonoBehaviour
         m_prevControllerMidpoint = m_controllerMidpoint;
         m_leftTrigger = false;
         m_rightTrigger = false;
-   
+        #endregion
 
-
+        #region World Scaling Handling
         m_controllerDistance = Vector3.Distance(m_leftController.transform.position, m_rightController.transform.position);
         if(m_leftGripped == true && m_rightGripped == true)
         {
@@ -83,31 +114,32 @@ public class InputManager : MonoBehaviour
                 if (m_controllerDistance > m_prevControllerDistance)
                 {
                     //ZOOM IN - SCALE UP
-                    Vector3 _newScale = new Vector3(m_vrRig.transform.localScale.x + m_scalingSpeed, m_vrRig.transform.localScale.y + m_scalingSpeed, m_vrRig.transform.localScale.z + m_scalingSpeed);
+                    Vector3 _newScale = new Vector3(m_gameEnvironment.transform.localScale.x + m_scalingSpeed, m_gameEnvironment.transform.localScale.y + m_scalingSpeed, m_gameEnvironment.transform.localScale.z + m_scalingSpeed);
                     if (_newScale.x > m_largestScale.x)
                     {
                         _newScale = m_largestScale;
                     }
-                    m_vrRig.transform.localScale = Vector3.Lerp(m_vrRig.transform.localScale, _newScale, 0.99f);
-                    m_vrRig.transform.position = Vector3.Lerp(m_vrRig.transform.position, new Vector3(m_vrRig.transform.position.x, m_vrRig.transform.position.y - 1f, m_vrRig.transform.position.z), 0.99f);
-                    if (m_vrRig.transform.position.y < -25)
+                    m_gameEnvironment.transform.localScale = Vector3.Lerp(m_gameEnvironment.transform.localScale, _newScale, 0.99f);
+                    m_gameEnvironment.transform.position = Vector3.Lerp(m_gameEnvironment.transform.position, new Vector3(m_gameEnvironment.transform.position.x, m_gameEnvironment.transform.position.y - m_scalingSpeed, m_gameEnvironment.transform.position.z), 0.99f);
+                    if (m_gameEnvironment.transform.position.y < -(m_largestScale.x/2))
                     {
-                        m_vrRig.transform.position = Vector3.Lerp(m_vrRig.transform.position, new Vector3(m_vrRig.transform.position.x, -25, m_vrRig.transform.position.z), 0.99f);
+                        m_gameEnvironment.transform.position = Vector3.Lerp(m_gameEnvironment.transform.position, new Vector3(m_gameEnvironment.transform.position.x, -(m_largestScale.x / 2), m_gameEnvironment.transform.position.z), 0.99f);
                     }
                 }
                 if (m_controllerDistance < m_prevControllerDistance)
                 {
                     //ZOOM OUT - SCALE DOWN
-                    Vector3 _newScale = new Vector3(m_vrRig.transform.localScale.x - m_scalingSpeed, m_vrRig.transform.localScale.y - m_scalingSpeed, m_vrRig.transform.localScale.z - m_scalingSpeed);
+                    Vector3 _newScale = new Vector3(m_gameEnvironment.transform.localScale.x - m_scalingSpeed, m_gameEnvironment.transform.localScale.y - m_scalingSpeed, m_gameEnvironment.transform.localScale.z - m_scalingSpeed);
                     if (_newScale.x < m_smallestScale.x)
                     {
                         _newScale = m_smallestScale;
                     }
-                    m_vrRig.transform.localScale = Vector3.Lerp(m_vrRig.transform.localScale, _newScale, 0.99f);
-                    m_vrRig.transform.position = Vector3.Lerp(m_vrRig.transform.position, new Vector3(m_vrRig.transform.position.x, m_vrRig.transform.position.y + 1f, m_vrRig.transform.position.z), 0.99f);
-                    if(m_vrRig.transform.position.y > 0)
+                    m_gameEnvironment.transform.localScale = Vector3.Lerp(m_gameEnvironment.transform.localScale, _newScale, 0.99f);
+                    m_vrRig.transform.position = Vector3.Lerp(m_vrRig.transform.position, Vector3.zero, 0.99f);
+                    m_gameEnvironment.transform.position = Vector3.Lerp(m_gameEnvironment.transform.position, new Vector3(m_gameEnvironment.transform.position.x, m_gameEnvironment.transform.position.y + m_scalingSpeed, m_gameEnvironment.transform.position.z), 0.99f);
+                    if(m_gameEnvironment.transform.position.y > m_maxWorldHeight)
                     {
-                        m_vrRig.transform.position = Vector3.Lerp(m_vrRig.transform.position, new Vector3(m_vrRig.transform.position.x, 0, m_vrRig.transform.position.z), 0.99f);
+                        m_gameEnvironment.transform.position = Vector3.Lerp(m_gameEnvironment.transform.position, new Vector3(m_gameEnvironment.transform.position.x, m_maxWorldHeight, m_gameEnvironment.transform.position.z), 0.99f);
                     }
 
                 }
@@ -116,6 +148,30 @@ public class InputManager : MonoBehaviour
         m_prevControllerDistance = m_controllerDistance;
         m_leftGripped = false;
         m_rightGripped = false;
+        #endregion
+
+
+        #region Point and Click Handling
+        //Debug.DrawRay(m_mainController.transform.position, m_mainController.transform.forward, Color.blue);
+
+
+        m_mainPointer.SetPosition(0, m_mainController.transform.position);
+        m_mainPointer.SetPosition(1, m_mainController.transform.forward * 100);
+
+        RaycastHit _hit;
+        if (Physics.Raycast(m_mainController.transform.position, m_mainController.transform.forward, out _hit, 100))
+        {
+            m_mainPointer.startColor = Color.green;
+            m_mainPointer.endColor = Color.green;
+        }
+        else // 3
+        {
+            m_mainPointer.startColor = Color.red;
+            m_mainPointer.endColor = Color.red;
+        }
+
+
+        #endregion
 
 
 
