7ec262f5a1035c0843d3aca27bbd25a9a730c969
Jack Griffiths <32262182+JackDotGriffiths@users.noreply.github.com>
2020-02-04 13:32:32 +0000

    S3C10 - Units Class
    Units Class has started to be created, this will track the statistics of all of the units within a hex.

---
+++ High Ground VR/Assets/Scripts/Classes/Units/Units.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units
+{
+
+    public float health;
+    public float damage;
+    public float intelligence;
+    public float timePerception = 1.0f;
+    public GameObject helmet;
+    public GameObject weapon;
+    public Color unitColour;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
