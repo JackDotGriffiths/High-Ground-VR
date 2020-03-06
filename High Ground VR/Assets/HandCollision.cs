85c04b25ce1eefd1ba0a0d1ec1ae3689be6a6011
Jack Griffiths <jackdotgriffiths@gmail.com>
2020-03-06 20:56:11 +0000

    S7C13 - Left Hand Controller Collisions
    Left Hand Controller Collisions added. This stops the hands going through a collider, and instead they just stay where they are and then move once the players hand is out of a collider. This needs to be done on the right hand also.

---
+++ High Ground VR/Assets/HandCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollision : MonoBehaviour
+{
+    public bool inCollider = false;
+    public Vector3 enterPosition;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        enterPosition = this.transform.position;
+        inCollider = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inCollider = false;
+    }
+}
---
+++ High Ground VR/Assets/HandPhysicsReaction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPhysicsReaction : MonoBehaviour
+{
+    [SerializeField] private GameObject m_inputPosition;
+
+    void FixedUpdate()
+    {
+
+       if( m_inputPosition.GetComponent<HandCollision>().inCollider == false)
+        {
+            this.transform.position = m_inputPosition.transform.position;
+            this.transform.rotation = m_inputPosition.transform.rotation;
+        }
+        else
+        {
+            this.transform.position = m_inputPosition.GetComponent<HandCollision>().enterPosition;
+            this.transform.rotation = m_inputPosition.transform.rotation;
+        }
+    }
+
+}
