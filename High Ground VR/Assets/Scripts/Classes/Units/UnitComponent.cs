2aa2b63350d9b603e641ad5e83dc0e0f119c7597
Jack Griffiths <jackdotgriffiths@gmail.com>
2020-03-12 11:19:08 +0000

    S8C8 - Health Bars Done
    Tank Health bars added.

--- High Ground VR/Assets/Scripts/Classes/Units/UnitComponent.cs
+++ High Ground VR/Assets/Scripts/Classes/Units/UnitComponent.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitComponent : MonoBehaviour
 {
+    #region Variables
     [Header("This component is used for both ordinary and large enemies.")]
     [SerializeField, Tooltip("Body object of the unit. Used to change material")] private MeshRenderer m_unitBody;
+    [SerializeField, Tooltip("Canvas on the enemy. Used for health bars, and other status updates.")] private Canvas m_enemyDisplay;
+    [SerializeField, Tooltip("The health bar Image for the unit. Only used for Tank enemies.")] private Image m_healthBar;
 
 
-    [SerializeField, Tooltip("The lowest possible health of the unit.")] private float m_lowestHealthPlayer;
+    [SerializeField,Space(10), Tooltip("The lowest possible health of the unit.")] private float m_lowestHealthPlayer;
     [SerializeField, Tooltip("The highest possible health of the unit.")] private float m_highestHealthPlayer;
     [SerializeField, Space(3), Tooltip("The lowest possible health of the unit.")] private float m_lowestHealthEnemy;
     [SerializeField, Tooltip("The highest possible health of the unit.")] private float m_highestHealthEnemy;
@@ -27,10 +31,33 @@ public class UnitComponent : MonoBehaviour
 
     [SerializeField,Space(10), Tooltip("All possible helmets for the units to spawn with.")] private List<GameObject> m_helmets;
     [SerializeField, Tooltip("All possible weapons for the units to spawn with.")] private List<GameObject> m_weapons;
+    #endregion
+
+
+    private float m_maxHealth = 0.0f;
 
     public Unit unit;
 
 
+
+    void Update()
+    {
+        try
+        {
+            m_enemyDisplay.transform.LookAt(Camera.main.transform);
+            if(InputManager.Instance.CurrentSize == InputManager.SizeOptions.large)
+            {
+                m_enemyDisplay.transform.eulerAngles = new Vector3(m_enemyDisplay.transform.eulerAngles.x, m_enemyDisplay.transform.eulerAngles.y, m_enemyDisplay.transform.eulerAngles.z);
+            }
+            else
+            {
+                m_enemyDisplay.transform.eulerAngles = new Vector3(0.0f, m_enemyDisplay.transform.eulerAngles.y, 0.0f);
+            }
+            m_healthBar.fillAmount = Mathf.Lerp(m_healthBar.fillAmount, unit.health / m_maxHealth, 1.5f * Time.deltaTime);
+        }
+        catch { }
+
+    }
 
     public void playerUnitConstructor()
     {
@@ -47,6 +74,7 @@ public class UnitComponent : MonoBehaviour
     {
         unitTypes _unitType = unitTypes.player;
         float _health = Random.Range(m_lowestHealthEnemy, m_highestHealthEnemy);
+        m_maxHealth = _health;
         float _damage = Random.Range(m_lowestDamageEnemy, m_highestDamageEnemy);
         GameObject _helmet = m_helmets[Random.Range(0, m_helmets.Count)];
         GameObject _weapon = m_weapons[Random.Range(0, m_weapons.Count)];
@@ -56,7 +84,9 @@ public class UnitComponent : MonoBehaviour
     }
 
 
-    //Kills the unit
+    /// <summary>
+    /// Kills the unit
+    /// </summary>
     public void Die()
     {
         //Instantiate a duplicate of the object
