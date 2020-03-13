e3028d249a72d47c00298fbc930d906df3197c81
Jack Griffiths <jackdotgriffiths@gmail.com>
2020-03-13 10:43:42 +0000

    S8C14 - Audio Manager Fixes + Comments
    Audio Manager is now much more usable, and contains comments to help with calling sounds from around the project.

--- High Ground VR/Assets/Scripts/Classes/Sound.cs
+++ High Ground VR/Assets/Scripts/Classes/Sound.cs
@@ -4,19 +4,14 @@ using UnityEngine.Audio;
 [System.Serializable]
 public class Sound
 {
-    public string name;
-
-    public AudioClip clip;
-
-    [Range(0f, 1f)]
-    public float volume;
-    [Range(0.1f, 3f)]
-    public float pitch;
-    [Range(0, 256)]
-    public int priority;
-
-    public bool loop;
-
-    [HideInInspector]
-    public AudioSource source;
+    public string name; //Name of the audio clip, used to call and play it.
+    public string description; //Optional description of the audio clip. Helps with working out which is which later on
+    public AudioClip clip; //Clip file itself
+    [Range(0f, 1f)] public float volume; //Volume to play clip at
+    [Range(0.1f, 3f)] public float pitch; //Pitch to play clip at 
+    [Range(0, 256)] public int priority; //Priority of clip 
+    [Range(0.0f, 1.0f)] public float spatialBlend; //Spatial blend of clip
+    public int minDistance; // Min distance of the 3D audio
+    public int maxDistance; // Max distance of the 3D audio
+    public bool loop; //Whether or not to Loop.
 }
--- High Ground VR/Assets/Scripts/Management/AudioManager.cs
+++ High Ground VR/Assets/Scripts/Management/AudioManager.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+
+public enum AudioMixers {UI, Effects, Music}
 public class AudioManager : MonoBehaviour
 {
 
     private static AudioManager _instance;
+    //Audio Mixers
+    [SerializeField, Tooltip("UI Audio Mixer")] private AudioMixerGroup m_UIMixer;
+    [SerializeField, Tooltip("Effects Audio Mixer")] private AudioMixerGroup m_effectsMixer;
+    [SerializeField, Tooltip("Music Audio Mixer")] private AudioMixerGroup m_musicMixer;
 
+    //Sound List
+    [SerializeField, Header("Local Sounds"),Space(20)] private Sound[] m_sounds;
 
-
-    //sounds
-    [SerializeField, Header("Local Sounds")]
-    private Sound[] localSounds;
-    [SerializeField]
-    private AudioMixerGroup localGameSounds;
 
     public static AudioManager Instance { get => _instance; set => _instance = value; }
 
@@ -27,91 +29,74 @@ public class AudioManager : MonoBehaviour
         }
 
         _instance = this;
-
-        foreach (Sound s in localSounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.priority = s.priority;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.outputAudioMixerGroup = localGameSounds;
-            s.source.spatialBlend = 0;
-            s.source.loop = s.loop;
-        }
     }
 
-    public void PlayLocalSound(string name)
+    /// <summary>
+    /// Plays either a 2D or 3D sound on the passed in targetObject
+    /// </summary>
+    /// <param name="_name">Name of the clip to play.</param>
+    /// <param name="_mixer">Mixer on which to play it on</param>
+    /// <param name="_destroyAfterPlaying">Whether to destroy the audioSource after the clip has played.</param>
+    /// <param name="_playin2D">Whether to acknowledge the spatialBlend and min/max distance of the clip. If true, spatial blend will be set to 0.</param>
+    /// <param name="_targetObject">Target GameObject to attatch the audio source to.</param>
+    /// <param name="_pitchShiftAmount">An amount of pitch shift to apply to the audio source. If 0, no pitch shift will occur.</param>
+    public void PlaySound(string _name,AudioMixers _mixer, bool _destroyAfterPlaying, bool _playin2D,GameObject _targetObject,float _pitchShiftAmount)
     {
         Sound _sound = null;
-        foreach (Sound s in localSounds)
+        foreach (Sound s in m_sounds)
         {
-            if (s.name == name)
+            if (s.name == _name)
             {
                 _sound = s;
                 break;
             }
         }
-
         if (_sound == null)
         {
             Debug.LogError("Cannot Play \n No sound of name: \"" + name + "\" found");
             return;
         }
-        _sound.source.Play();
-    }
 
-    public void StopLocalSound(string name)
-    {
-        Sound _sound = null;
-        foreach (Sound s in localSounds)
+
+        AudioSource _source = _targetObject.AddComponent<AudioSource>(); //Attach to the target Object. Only really makes a difference if the sound is played in 3D.
+        _source.clip = _sound.clip;
+        _source.priority = _sound.priority;
+        _source.volume = _sound.volume;
+        _source.pitch = _sound.pitch + Random.Range(-_pitchShiftAmount,_pitchShiftAmount); //Pitch shifting, if 0 is passed in nothing will happen.
+
+        switch (_mixer) //Set the mixer that the audio clip is meant to play on.
         {
-            if (s.name == name)
-            {
-                _sound = s;
+            case AudioMixers.UI:
+                _source.outputAudioMixerGroup = m_UIMixer;
                 break;
-            }
-        }
-
-        if (_sound == null)
-        {
-            Debug.LogError("Cannot Stop \n No sound of name: \"" + name + "\" found");
-            return;
-        }
-
-        _sound.source.Stop();
-    }
-
-    public bool isLocalSoundPlaying(string name)
-    {
-        Sound _sound = null;
-        foreach (Sound s in localSounds)
-        {
-            if (s.name == name)
-            {
-                _sound = s;
+            case AudioMixers.Effects:
+                _source.outputAudioMixerGroup = m_effectsMixer;
                 break;
-            }
+            case AudioMixers.Music:
+                _source.outputAudioMixerGroup = m_musicMixer;
+                break;
         }
 
-        if (_sound == null)
+        if(_playin2D == true)  //Set spatialBlend based on 2D or 3D audio
         {
-            Debug.LogError("Cannot return time \n No sound of name: \"" + name + "\" found");
-            return false;
+            _source.spatialBlend = 0;
+        }
+        else
+        {
+            _source.spatialBlend = _sound.spatialBlend;
+            _source.minDistance = _sound.minDistance;
+            _source.maxDistance = _sound.maxDistance;
         }
 
-        return _sound.source.isPlaying;
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+        _source.loop = _sound.loop;
+        _source.Play();
 
-    // Update is called once per frame
-    void Update()
-    {
+        if(_destroyAfterPlaying == true) //Destroys the source after playing. Useful for spot effects.
+        {
+            Destroy(_source, _sound.clip.length);
+        }
 
     }
 }
